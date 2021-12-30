7533de2b481067aeefd928bbb4c5050527a066fe
John Doe <johndoe@example.com>
2021-12-30 15:35:48 +0300

    main
    Signed-off-by: John Doe <johndoe@example.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
-const { merge } = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-    mode: 'production',
+    mode: "production",
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
